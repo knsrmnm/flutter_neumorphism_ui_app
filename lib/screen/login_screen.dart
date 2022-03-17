0df01a77f6215be6b123f5bf764bd147d330400e
Ken Minami <kenminami.dev@gmail.com>
2022-03-17 23:20:14 +0900

    Update login_screen.dart

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -9,26 +9,57 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _check1 = false;
+  bool _check2 = false;
+  bool _check3 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: NeumorphicTheme.baseColor(context),
-      body: Center(
-        child: NeumorphicButton(
-            margin: EdgeInsets.only(top: 12),
-            onPressed: (){},
-            style: NeumorphicStyle(
-              shape: NeumorphicShape.flat,
-              boxShape:
-              NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
-              //border: NeumorphicBorder()
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment(0, -0.5),
+              child: Text('Check Box', style: TextStyle(fontSize: 18.0)),
             ),
-            padding: const EdgeInsets.all(12.0),
-            child: Text(
-              "Click Me!",
-              style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.bold),
-            )),
-      ),
+            Align(
+              alignment: Alignment(0, -0.3),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  NeumorphicCheckbox(
+                    value: _check1,
+                    onChanged: (value) {
+                      setState(() {
+                        _check1 = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(width: 50.0),
+                  NeumorphicCheckbox(
+                    value: _check2,
+                    onChanged: (value) {
+                      setState(() {
+                        _check2 = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(width: 50.0),
+                  NeumorphicCheckbox(
+                    value: _check3,
+                    onChanged: (value) {
+                      setState(() {
+                        _check3 = value;
+                      });
+                    },
+                  ),
+                ],
+              )
+            ),
+          ],
+        )
+      )
     );
   }
 }
